5b7de0e877aab8297248b43ef786e1979460e010
GauthamBanasandra <gautham.bangalore@gmail.com>
2017-09-07 11:55:00 +0530

    Add v8 inspector

---
+++ v8_inspector/sample.js
@@ -0,0 +1,13 @@
+function exponent(base, exp)
+{
+    var result = 1;
+    x = y;
+    for (var i=0; i < exp; i++) {
+        result *= base;
+    }
+    return result;
+}
+
+function exponent2(params) {
+	console.log(params);
+}
