d0739ca3d440763b010955e1805b97d33109bf83
GauthamBanasandra <gautham.bangalore@gmail.com>
2017-11-22 14:44:14 +0530

    Add delegate for BucketGet

--- v8proj/bucket.cpp
+++ v8proj/bucket.cpp
@@ -11,13 +11,13 @@
 #include <string>
 #include <vector>
 
-void Bucket() {
+Bucket::Bucket() {
   std::vector<std::string> bucket_names{"src", "dst"};
   auto isolate = v8::Isolate::GetCurrent();
   InstallMaps(isolate, bucket_names);
 }
 
-void InstallMaps(v8::Isolate *isolate,
+void Bucket::InstallMaps(v8::Isolate *isolate,
                  const std::vector<std::string> &bucket_names) {
   v8::HandleScope handle_scope(isolate);
   auto context = isolate->GetCurrentContext();
@@ -29,7 +29,7 @@ void InstallMaps(v8::Isolate *isolate,
   }
 }
 
-v8::Local<v8::Object> WrapBucketMap(v8::Isolate *isolate) {
+v8::Local<v8::Object> Bucket::WrapBucketMap(v8::Isolate *isolate) {
   v8::EscapableHandleScope handle_scope(isolate);
   v8::Global<v8::ObjectTemplate> bucket_map_template;
 
@@ -40,34 +40,52 @@ v8::Local<v8::Object> WrapBucketMap(v8::Isolate *isolate) {
 
   auto templ = v8::Local<v8::ObjectTemplate>::New(isolate, bucket_map_template);
   auto obj = templ->NewInstance(isolate->GetCurrentContext()).ToLocalChecked();
-
   return handle_scope.Escape(obj);
 }
 
-v8::Local<v8::ObjectTemplate> MakeBucketMapTemplate(v8::Isolate *isolate) {
+v8::Local<v8::ObjectTemplate> Bucket::MakeBucketMapTemplate(v8::Isolate *isolate) {
   v8::EscapableHandleScope handle_scope(isolate);
 
   auto obj_templ = v8::ObjectTemplate::New(isolate);
   obj_templ->SetHandler(v8::NamedPropertyHandlerConfiguration(
-      BucketGet, BucketSet, nullptr, BucketDelete));
+      BucketGetDelegate, BucketSet, nullptr, BucketDelete));
   obj_templ->SetIndexedPropertyHandler(
-      v8::IndexedPropertyGetterCallback(BucketGet),
+      v8::IndexedPropertyGetterCallback(BucketGetDelegate),
       v8::IndexedPropertySetterCallback(BucketSet), nullptr,
       v8::IndexedPropertyDeleterCallback(BucketDelete));
   return handle_scope.Escape(obj_templ);
 }
 
-void BucketGet(v8::Local<v8::Name> name,
-               const v8::PropertyCallbackInfo<v8::Value> &info) {
+// Using a template to allow access to names and indexes.
+template<typename T>
+void Bucket::BucketGetDelegate(T name,
+                                 const v8::PropertyCallbackInfo<v8::Value> &info) {
+  std::cout << "Called bucket get delegate" << std::endl;
+  BucketGet<T>(name, info);
+}
+
+template<>
+void Bucket::BucketGet<v8::Local<v8::Name>>(v8::Local<v8::Name> name, const v8::PropertyCallbackInfo<v8::Value> &info) {
+  std::cout << "Called bucket get" << std::endl;
+  v8::String::Utf8Value utf8(name);
+  std::cout << *utf8 << std::endl;
+}
+
+template<>
+void Bucket::BucketGet<uint32_t>(uint32_t name, const v8::PropertyCallbackInfo<v8::Value> &info) {
+  auto name_v8_str = v8::String::NewFromUtf8(info.GetIsolate(), std::to_string(name).c_str());
+  v8::Local<v8::Name> name_name(name_v8_str);
+  v8::String::Utf8Value name_utf8(name_name);
+  std::cout << *name_utf8 << std::endl;
   std::cout << "Called bucket get" << std::endl;
 }
 
-void BucketSet(v8::Local<v8::Name> name, v8::Local<v8::Value> value_obj,
+void Bucket::BucketSet(v8::Local<v8::Name> name, v8::Local<v8::Value> value_obj,
                const v8::PropertyCallbackInfo<v8::Value> &info) {
   std::cout << "Called bucket set" << std::endl;
 }
 
-void BucketDelete(v8::Local<v8::Name> name,
+void Bucket::BucketDelete(v8::Local<v8::Name> name,
                   const v8::PropertyCallbackInfo<v8::Boolean> &info) {
   std::cout << "Called bucket delete" << std::endl;
 }
--- v8proj/bucket.hpp
+++ v8proj/bucket.hpp
@@ -11,16 +11,25 @@
 
 #include <v8.h>
 
-void BucketGet(v8::Local<v8::Name> name,
-               const v8::PropertyCallbackInfo<v8::Value> &info);
-void BucketSet(v8::Local<v8::Name> name, v8::Local<v8::Value> value_obj,
-               const v8::PropertyCallbackInfo<v8::Value> &info);
-void BucketDelete(v8::Local<v8::Name> name,
-                  const v8::PropertyCallbackInfo<v8::Boolean> &info);
-v8::Local<v8::ObjectTemplate> MakeBucketMapTemplate(v8::Isolate *isolate);
-v8::Local<v8::Object> WrapBucketMap(v8::Isolate *isolate);
-void InstallMaps(v8::Isolate *isolate,
-                 const std::vector<std::string> &bucket_names);
-void Bucket();
+class Bucket {
+public:
+  Bucket();
+  template<typename T>
+  static void BucketGetDelegate(T name,
+                           const v8::PropertyCallbackInfo<v8::Value> &info);
+  template<typename>
+  static void BucketGet(v8::Local<v8::Name> name, const v8::PropertyCallbackInfo<v8::Value> &info);
+  template<typename>
+  static void BucketGet(uint32_t name, const v8::PropertyCallbackInfo<v8::Value> &info);
+  
+  static void BucketSet(v8::Local<v8::Name> name, v8::Local<v8::Value> value_obj,
+                 const v8::PropertyCallbackInfo<v8::Value> &info);
+  static void BucketDelete(v8::Local<v8::Name> name,
+                    const v8::PropertyCallbackInfo<v8::Boolean> &info);
+  v8::Local<v8::ObjectTemplate> MakeBucketMapTemplate(v8::Isolate *isolate);
+  v8::Local<v8::Object> WrapBucketMap(v8::Isolate *isolate);
+  void InstallMaps(v8::Isolate *isolate,
+                   const std::vector<std::string> &bucket_names);
+};
 
 #endif /* bucket_hpp */
